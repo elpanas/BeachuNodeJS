2ca0e76ee8d511e5df762a0bdd8148e095577dc7
LucaPanas <49339752+elpanas@users.noreply.github.com>
2020-05-04 21:34:09 +0200

    Update restuser.js

--- routes/restuser.js
+++ routes/restuser.js
@@ -26,7 +26,7 @@ router.get('/login', (req, res) => {
             if (!result)
                 res.status(401).set('WWW-Authenticate','Basic: "Area Riservata"').send();
             else
-                res.status(200).set('Content-type','application/json').send(result);
+                res.status(200).json(result);
         })
         .catch(() => { res.status(401).send('Error'); })
 });
