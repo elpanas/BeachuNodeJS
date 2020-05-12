35fad8ed9aa53b235be50e66224c820f7f7132f2
LucaPanas <49339752+elpanas@users.noreply.github.com>
2020-05-12 22:56:13 +0200

    Update reststab.js

--- routes/reststab.js
+++ routes/reststab.js
@@ -44,7 +44,7 @@ router.get('/gest', (req, res) => {
                 else
                     res.status(404).send('Bathing establishments were not found');
             })
-            .catch(() => { res.status(404).send('Bathing establishments were not found') });
+            .catch(() => { res.status(400).send() });
     }
     else
         res.status(401).setHeader('WWW-Authenticate', 'Basic realm: "Area Riservata"').send(); 
