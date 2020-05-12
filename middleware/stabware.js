88b3c3859c4766afc64f89e23b838bf3dc8f11be
LucaPanas <49339752+elpanas@users.noreply.github.com>
2020-05-12 23:10:09 +0200

    Update stabware.js

--- middleware/stabware.js
+++ middleware/stabware.js
@@ -60,11 +60,11 @@ async function getStabDispCoord(long, lat) {
 async function getStabGest(auth) {
 
     const tmp = auth.split(' ');   // Divido in base allo stazio  "Basic Y2hhcmxlczoxMjM0NQ==" per recuperare la 2a parte
-    const idu = Buffer.from(tmp[1], 'base64').toString(); // creo un buffer e lo avviso che l'input è in base64  
+    const idu = Buffer.from(tmp[1], 'base64').toString(); // creo un buffer e lo avviso che l'input Ã¨ in base64  
 
     return await Stabilimento
         .find({ idutente: idu }) // criteri di ricerca          
-        .sort({ nome: 1 }) // ordine asc
+        .sort({ nome: 1 }); // ordine asc
 }
 
 // RIMUOVE UNO STABILIMENTO
