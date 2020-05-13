4819f8a312b9de73406e268753df45aa349b0b47
LucaPanas <49339752+elpanas@users.noreply.github.com>
2020-05-13 21:54:51 +0200

    Update utenteware.js

--- middleware/utenteware.js
+++ middleware/utenteware.js
@@ -19,7 +19,7 @@ async function createUtente(dati_utente) {
     const result = await utente.save();
 
     if (result._id)
-        return result._id;
+        return Buffer.from(result._id, 'base64').toString;
     else
         return false;
 }
