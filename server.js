17c0b8ecd2dd9166733a803f7d638091c1328df4
El Panas <luca.panariello@gmail.com>
2021-06-25 13:44:13 +0200

    tidy up code

--- server.js
+++ server.js
@@ -1,31 +1,20 @@
-require('dotenv').config();
-const express = require('express'); // framework nodejs
+require('./db/db'); // DATABASE CONNECTIONS
+const express = require('express'); // FRAMEWORK
 const app = express(),
-  compression = require('compression'),
+  config = require('./config/config'), // CONFIGURATIONS
+  compression = require('compression'), // MIDDLEWARES
   helmet = require('helmet'),
-  mongoose = require('mongoose'), // framework per mongoDB
-  restbath = require('./routes/restbath'),
-  url = process.env.DB_URI,
-  port = process.env.PORT || 3000,
-  connectionOptions = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    autoIndex: false,
-    useFindAndModify: false,
-  };
+  restbath = require('./routes/restbath'); // ROUTES
 
+// MIDDLEWARES ACTIVACTION
 app.use(helmet());
 app.use(compression());
 app.use(express.json());
 
-// db connection
-mongoose
-  .connect(url, connectionOptions)
-  .then(() => console.log('Connected to MongoDB...'))
-  .catch((err) => console.error('Could not connect to MongoDB...', err));
-
+// ROUTES
 app.get('/', (req, res) => res.send('BeachU Web Service'));
 app.use('/api/bath', restbath);
 
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+app.listen(config.app.port, () =>
+  console.log(`Listening on port ${config.app.port}...`)
+);
